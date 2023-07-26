2c26334c35c3aa465f4148f3056cd084aa49f8ca
greypeg <greg.papas@ottofinance.io>
2023-07-26 15:22:45 +0100

    replaced clerk with nextAuth, fixed import of icons and added buttons instead of cards at the home page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+import { Layout } from "~/layout/page-layout";
+import { ThemeProvider } from "~/theme/theme.provider";
+
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider >
+    </SessionProvider >
+  );
+};
+
+export default api.withTRPC(MyApp);
